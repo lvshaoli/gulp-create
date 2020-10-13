3d6478fe6f9c0e50735e2511161c9db7159f232a
lvshaoli <lvshaoligood@126.com>
2020-10-13 17:05:51 +0800

    添加 useref

--- gulpfile.js
+++ gulpfile.js
@@ -51,23 +51,26 @@ const data = {
   }
 
   const clean = () => {
-      return del(['dist'])
+      return del(['dist', 'temp'])
   }
 const style = () => {
     return src('src/assets/styles/*.scss', {base: 'src'})
             .pipe(plugins.sass({ outputStyle: 'expanded' }))
-            .pipe(dest('dist'))
+            .pipe(dest('temp'))
+            .pipe(bs.reload({ stream: true }))
 }
 
 const script = () => {
     return src('src/assets/scripts/*.js', { base: 'src' })
             .pipe(plugins.babel({ presets: ['@babel/preset-env'] }))
-            .pipe(dest('dist'))
+            .pipe(dest('temp'))
+            .pipe(bs.reload({ stream: true }))
 }
 const page = () => {
     return src('src/*.html', { base: 'src' })
             .pipe(plugins.swig({ data, default: { cache: false } }))
-            .pipe(dest('dist'))
+            .pipe(dest('temp'))
+            .pipe(bs.reload({ stream: true }))
 }
 
 const image = () => {
@@ -99,7 +102,7 @@ const serve = () => {
         notify: false,
         port: 2080,
         server: {
-            baseDir: ['src'],
+            baseDir: ['temp', 'src', 'public'],
             routes: {
                 '/node_modules': 'node_modules'
             }
@@ -108,10 +111,23 @@ const serve = () => {
     })
 }
 
+const useref = () => {
+  return src('temp/*.html', { base: 'temp' })
+        .pipe(plugins.useref({ searchPath: ['temp', '.'] }))
+        .pipe(plugins.if(/\.js$/, plugins.uglify()))
+        .pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+        .pipe(plugins.if(/\.html$/, plugins.htmlmin({
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true
+        })))
+        .pipe(dest('dist'))
+}
+
 const compile = parallel(style, script, page)
 const build = series(clean,
      parallel(
-    compile,
+    series(compile, useref),
     image,
     font,
     extra
